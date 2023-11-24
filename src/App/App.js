2f8b32769f37d4a4483d87dd9c75203516ad696e
shwetapund <pundshweta2020@gmail.com>
2023-11-24 15:46:52 -0800

    basic design is ready for tic-tac-toe

---
+++ src/App/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-center">🐱 Tic Tac Toe 🐒</h1>
+
+            <div className="board-container">
+                <div className="row">
+                    <div className="box"></div>
+                    <div className="box"></div>
+                    <div className="box"></div>
+                </div>
+                <div className="row">
+                    <div className="box"></div>
+                    <div className="box"></div>
+                    <div className="box"></div>
+                </div>
+                <div className="row">
+                    <div className="box"></div>
+                    <div className="box"></div>
+                    <div className="box"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import react from 'react'
 import ReactDOM from "react-dom/client"
+import './index.css'
+import App from "./App/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
     <>
-    <Home />
+    <App />
     </>
 )
+
