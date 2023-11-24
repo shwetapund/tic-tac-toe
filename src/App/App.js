ef555ee96990bd0822f34ac8d01d7b4b5c8d99a1
shwetapund <pundshweta2020@gmail.com>
2023-11-24 06:23:04 -0800

    few code are added

--- src/App/App.js
+++ src/App/App.js
@@ -1,29 +1,139 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
+function App(){
+    const [player, setPlayer] = useState(1);
+    const [board, setBoard] = useState({
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: "",
+        6: "",
+        7: "",
+        8: "",
+        9: "",
+    })
+    const [winner, setWinner] = useState(null);
+
+    const checkWinner = ()=>{
+        console.log(player);
+        console.log(board)
+
+        const symbol = player === 1 ? "🐱" : "🐒";
+
+        if(board[1] === symbol && board[2] === symbol && board[3] === symbol){
+           setWinner(player);
+        }
+        else if(board[4] === symbol && board[5] === symbol && board[6] === symbol){
+            setWinner(player);
+        }
+        else if(board[7] === symbol && board[8] === symbol && board[9] === symbol){
+            setWinner(player);
+        }
+        else if(board[1] === symbol && board[4] === symbol && board[7] === symbol){
+            setWinner(player);
+        }
+        else if(board[2] === symbol && board[5] === symbol && board[8] === symbol){
+            setWinner(player);
+        }
+        else if(board[3] === symbol && board[6] === symbol && board[9] === symbol){
+            setWinner(player);
+        }
+        else if(board[1] === symbol && board[5] === symbol && board[9] === symbol){
+            setWinner(player);
+        }
+        else if(board[3] === symbol && board[5] === symbol && board[7] === symbol){
+            setWinner(player);
+        }
+        
+        setPlayer(player === 1 ? 2 : 1);
+
+    }
+
+    const play = (boxNo) =>{
+        if(board[boxNo] !== "" || winner !== null){
+            return;
+        }
+        if(player===1){
+            setBoard({...board, [boxNo]: "🐱"})
+        }
+        else{
+            setBoard({...board, [boxNo]: "🐒"})
+        }
+
+      
+      
+    }
+
+    useEffect(()=>{
+        checkWinner(player);
+    },[board])
+
     return (
         <div>
-            <h1 className="text-center">🐱 Tic Tac Toe 🐒</h1>
+            {/* <h1>🐹Tic Tac Toe🦊</h1> */}
+            <h1 className="text-center"> Tic Tac Toe </h1>
+
+            <div className="player-container">
+                <span className="player-title">
+                    Player 1: 🐒
+                </span>
+                <span className="player-title">
+                    Player 2: 🐱
+                </span>
+            </div>
+
+            <p className="text-center current-player">
+                Current Player: {player === 1 ? "🐱" : "🐒"}
+            </p>
+
+            {
+                winner ?
+                (
+                    <h1>Winner is {player === 1 ? "🐱" : "🐒"}</h1>
+                ) : null
+            }
 
             <div className="board-container">
                 <div className="row">
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
+                    <div className="box" onClick={()=>{play(1) }}>
+                        {board[1]}
+                    </div>
+                    <div className="box" onClick={()=>{play(2)}}>
+                    {board[2]}
+                    </div>
+                    <div className="box" onClick={()=>{play(3) }}>
+                    {board[3]}
+                    </div>
                 </div>
                 <div className="row">
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
+                    <div className="box" onClick={()=>{ play(4) }}>
+                    {board[4]}
+                    </div>
+                    <div className="box" onClick={()=>{play(5) }}>
+                    {board[5]}
+                    </div>
+                    <div className="box" onClick={()=>{ play(6)  }}>
+                    {board[6]}
+                    </div>
                 </div>
                 <div className="row">
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
+                    <div className="box" onClick={()=>{play(7) }}>
+                    {board[7]}
+                    </div>
+                    <div className="box" onClick={()=>{play(8)}}>
+                    {board[8]}
+                    </div>
+                    <div className="box" onClick={()=>{ play(9) }}>
+                    {board[9]}
+                    </div>
                 </div>
             </div>
+
+            <div></div>
         </div>
     )
 }
-export default App
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import react from 'react'
 import ReactDOM from "react-dom/client"
 import './index.css'
 import App from "./App/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(
-    <>
-    <App />
-    </>
-)
+root.render(<App />)
 
